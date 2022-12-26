56e9fd09522a76a79d16111f0d01ecd34e792541
x20220914 <x20220914@gmail.com>
2022-12-26 12:30:23 +0000

    initial example application

--- build.sh
+++ build.sh
@@ -2,8 +2,24 @@
 
 sudo apt install -y gcc-arm-none-eabi
 
-wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/sdks/nrf5/binaries/nrf5_sdk_17.1.0_ddde560.zip && unzip nrf5_sdk_17.1.0_ddde560.zip && rm nrf5_sdk_17.1.0_ddde560.zip
+PREV_DIR=$(pwd)
 
-# current softdevice in SDK
-# wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/softdevices/s112/s112nrf52720.zip && unzip s112nrf52720.zip && rm s112nrf52720.zip
+# get Nordic SDK, if it is not there
+cd nRF5_SDK_17.1.0_ddde560/examples/ble_peripheral/ble_app_hids_keyboard/pca10040e/s112/armgcc || \
+ wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/sdks/nrf5/binaries/nrf5_sdk_17.1.0_ddde560.zip && \
+ unzip nrf5_sdk_17.1.0_ddde560.zip && rm nrf5_sdk_17.1.0_ddde560.zip && rm nrf5_sdk_17.1.0_ddde560.zip
+ 
+# patch nRF5_SDK_17.1.0_ddde560/examples/ble_peripheral/ble_app_hids_keyboard
+# set values in Makefile.posix according to the actual configuration of your system
+cd $PREV_DIR
+cp Makefile.posix nRF5_SDK_17.1.0_ddde560/components/toolchain/gcc/Makefile.posix
+cp main.c nRF5_SDK_17.1.0_ddde560/examples/ble_peripheral/ble_app_hids_keyboard/main.c
+cp bsp.c nRF5_SDK_17.1.0_ddde560/components/libraries/bsp/bsp.c
+cp bsp.h nRF5_SDK_17.1.0_ddde560/components/libraries/bsp/bsp.h
+cp pca10040_freemote.h nRF5_SDK_17.1.0_ddde560/components/boards/pca10040.h
+cp bsp_btn_ble.c nRF5_SDK_17.1.0_ddde560/components/libraries/bsp/bsp_btn_ble.c
+cp bsp_config.h nRF5_SDK_17.1.0_ddde560/components/libraries/bsp/bsp_config.h
+cp sdk_config.h nRF5_SDK_17.1.0_ddde560/examples/ble_peripheral/ble_app_hids_keyboard/pca10040e/s112/config/sdk_config.h
 
+cd nRF5_SDK_17.1.0_ddde560/examples/ble_peripheral/ble_app_hids_keyboard/pca10040e/s112/armgcc
+make
